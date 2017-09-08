e68f5f67157e7484f03b9518a5d8e6f2a9c53986
Dayao Ji <jdy@rock-chips.com>
2017-09-08 14:03:56 +0800

    QLauncher: remove system suspend to pm service
    Change-Id: I66d6d1633f685096e84cad5187bf8219117e544d
    Signed-off-by: Dayao Ji <jdy@rock-chips.com>

--- src/launcher.cpp
+++ src/launcher.cpp
@@ -9,7 +9,6 @@ Launcher::Launcher(QObject *parent) : QObject(parent)
 
 void Launcher::slot_standby()
 {
-    system("echo mem > /sys/power/state");
 }
 
 void Launcher::pickWallpaper()
@@ -18,17 +17,6 @@ void Launcher::pickWallpaper()
 
 void Launcher::powerControl()
 {
-    if(rk_get_screen_status())
-    {
-        qDebug()<<"key value is PowerKey, screen off ";
-        rk_screen_off();
-        QTimer::singleShot(100, this, SLOT(slot_standby()));
-    }
-    else
-    {
-        qDebug()<<"key value is PowerKey, screen ON ";
-        rk_screen_on();
-    }
 }
 
 void Launcher::emitNewIntent()

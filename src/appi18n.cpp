723c7bb16560d63e150f15dc6416d3671df1d71a
Guochun Huang <hero.huang@rock-chips.com>
2017-12-13 12:46:14 +0800

    QLauncher: get the default language from the LC_ALL variable
    Change-Id: I3bd4af7b789604a140dc1b56cabb34940dda912a
    Signed-off-by: Guochun Huang <hero.huang@rock-chips.com>

--- src/appi18n.cpp
+++ src/appi18n.cpp
@@ -22,10 +22,13 @@ QSettings* AppI18n::getSettings(){
 }
 
 QString AppI18n::getName(Application* app){
+
+    const int langTokenLength = 2; /*FIXME: is checking two chars enough?*/
     QSettings* setting= m_map.value(app,NULL);
+    QVariant defaultLang(QLocale::system().name()/*"zh_CN.UTF-8"*/);
     if(setting!=NULL){
         QSettings* tmp= getSettings();
-        QString lang= tmp->value("LANG").toString()+"/appName";
+	 QString lang= tmp->value("LANG",defaultLang).toString().leftRef(langTokenLength)+"/appName";
         delete tmp;
         return setting->value(lang).toString();
     }
@@ -33,12 +36,14 @@ QString AppI18n::getName(Application* app){
 
 void AppI18n::reflush(){
 
+    const int langTokenLength = 2; /*FIXME: is checking two chars enough?*/
     QMapIterator<Application*,QSettings*> iter(m_map);
+    QVariant defaultLang(QLocale::system().name()/*"zh_CN.UTF-8"*/);
     while(iter.hasNext())
     {
         iter.next();
         QSettings* tmp= getSettings();
-        QString lang= tmp->value("LANG").toString()+"/appName";
+        QString lang= tmp->value("LANG",defaultLang).toString().leftRef(langTokenLength)+"/appName";
         QString name= iter.value()->value(lang).toString();
         iter.key()->setUiName(name);
         delete tmp;
